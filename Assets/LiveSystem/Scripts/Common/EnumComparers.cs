be0729768109c390d93a0545b0f1b343f32b9101
Q <jjes20055114123@gmail.com>
2022-01-13 18:22:01 +0800

    refactor: rename Live2DParamIdComparer script to EnumComparers

--- Assets/LiveSystem/Scripts/Common/EnumComparers.cs
+++ Assets/LiveSystem/Scripts/Common/EnumComparers.cs
@@ -3,6 +3,8 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file
 
+using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace LiveSystem
