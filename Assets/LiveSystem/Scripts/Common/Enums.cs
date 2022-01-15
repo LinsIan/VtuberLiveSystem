5c78ea9d2f873aedeb8ee31ddf6264f41d4648b5
LinsIan <jjes20055114123@gmail.com>
2022-01-15 16:32:34 +0800

    feat: update Live2DParamId

--- Assets/LiveSystem/Scripts/Common/Enums.cs
+++ Assets/LiveSystem/Scripts/Common/Enums.cs
@@ -24,6 +24,7 @@ namespace LiveSystem
         ParamEyeLOpen,
         ParamEyeROpen,
         ParamEyeBallX,
+        ParamEyeBallY,
         ParamMouthOpenY,
         ParamBodyAngleX,
         ParamBodyAngleY,
