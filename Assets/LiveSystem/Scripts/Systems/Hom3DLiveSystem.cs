b921b5cfc0c7b4233e0bd3e5659bd2fedb03a4e7
Q <jjes20055114123@gmail.com>
2022-03-01 13:46:49 +0800

    test: add debug log for blend shape clip

--- Assets/LiveSystem/Scripts/Systems/Hom3DLiveSystem.cs
+++ Assets/LiveSystem/Scripts/Systems/Hom3DLiveSystem.cs
@@ -1,12 +1,24 @@
+// Copyright (c) 2021 Lins Ian
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mediapipe.Unity.Holistic;
+using VRM;
 
 namespace LiveSystem
 {
     public class Hom3DLiveSystem : LiveSystem
     {
+        //public BlendShapeKey key;
+
+        public BlendShapeClip clip;
+
+        //public BlendShapeBinding binding; 
+
         protected override IEnumerator InitSubSystem()
         {
             var newModelController = new Home3DModelController(modelData, LiveMode.FaceOnly);
@@ -20,6 +32,16 @@ namespace LiveSystem
             faceDataCalculater.OnFaceDataOutput += newModelController.OnFaceDataOutput;
             calculaters.Add(faceDataCalculater);
             modelController = newModelController;
+
+            Debug.Log(clip.Values.Length);
+            foreach (var item in clip.Values)
+            {
+                Debug.Log(item.ToString());
+            }
+
+            var key = clip.Key;
+            Debug.Log(key.Name);
+
 
             yield return base.InitSubSystem();
         }
